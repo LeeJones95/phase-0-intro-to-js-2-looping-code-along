f65e77e343e4b1c64983b1f1f89f8667ac8d9724
Leonna Jones <62804741+LeeJones95@users.noreply.github.com>
2021-06-18 15:02:12 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function wrapGifts(string, event){
+    newArray=[]
+    for(i=0; i < string.length;i++){
+        console.log(`Thank you, ${string[i]}, for the wonderful ${event} gift!`)
+        newArray.push(string[i])
+    }
+
+    return newArray
+}
+
+function countdown(n) {
+    let countdown = n;
+    while (countdown >= 0) {
+        console.log(countdown)
+        countdown--
+    }
+}
+
+
+countdown(10)
+wrapGifts(["Ada", "Brendan", "Ali"], "birthday")
