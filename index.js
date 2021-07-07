e385a95c349d4762be15f88c7dd92572432d3822
LeeJones95 <leonna.jones18@gmail.com>
2021-07-06 23:38:34 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 // Code your solutions in this file
-function wrapGifts(string, event){
-    newArray=[]
-    for(i=0; i < string.length;i++){
-        console.log(`Thank you, ${string[i]}, for the wonderful ${event} gift!`)
-        newArray.push(string[i])
-    }
+//function wrapGifts(string, event){
+  //  newArray=[]
+  //  for(i=0; i < wrapGifts.length;i++){
+  //      console.log(`Thank you, ${string[i]}, for the wonderful ${event} gift!`)
+  //      newArray.push(string[i])
+  //  }
 
-    return newArray
+  //  return newArray
+//}
+
+function countDown(n) {
+   let countdown = n;
+   while (countdown >= 0) {
+       console.log(countdown)
+       countdown--
+   }
 }
 
-function countdown(n) {
-    let countdown = n;
-    while (countdown >= 0) {
-        console.log(countdown)
-        countdown--
-    }
+//countdown(10)
+//wrapGifts(["Ada", "Brendan", "Ali"], "birthday")
+//writeCards(["Ada", "Brendan", "Ali"], "birthday"); 
+const cards = [ 'Lisa', 'Kaitlin', 'Jan' ];
+
+function writeCards(cards) {
+  for (let i = 0; i < cards.length; i++) {
+    cards[i] = `Thank you, ${cards[i]}, for the wonderful surprise gift!`;
+  }
+
+  return cards;
 }
 
-
-countdown(10)
-wrapGifts(["Ada", "Brendan", "Ali"], "birthday")
+writeCards (cards)
